316503a042670e64323fd7a3416641dab298e71a
Riftland <fran.mscr@gmail.com>
2017-11-03 14:21:54 +0100

    Add color black to text

--- Objects/Text.js
+++ Objects/Text.js
@@ -1,11 +1,10 @@
 function Text(players){
   this.ctx = ctx ;
-  this.x = 0;
-  this.y = 0;
   this.players = players;
 }
 
 Text.prototype.write = function(){
   ctx.font = "40px Permanent Marker";
+  ctx.fillStyle = "black";
   ctx.fillText("PJ1 - Red: " + this.players[0].lifes + " -- PJ2 - Green: " + this.players[1].lifes + " | EARLY ACCES v0.2", 10, 50);
 };
